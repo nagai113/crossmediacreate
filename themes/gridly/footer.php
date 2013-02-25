e54b57c4e7fd75116ab14aedf46822f2a1111d79
nagai113 <nagai@mousousakusen.com>
2013-02-25 21:57:22 +0900

    フッターにロゴ追加

--- themes/gridly/footer.php
+++ themes/gridly/footer.php
@@ -4,14 +4,12 @@
    <div id="footer-area">
 			<?php dynamic_sidebar( 'gridly_footer' ); ?>
         </div><!-- // footer area -->   
+             <img src="http://crossmediacreate.com/wp-content/uploads/adband_logo_wordpress_foot.jpg">
 <?php }  ?>     
       
-
-
  <div id="copyright">
  <p>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?><a href="<?php echo esc_url( __('http://www.eleventhemes.com/', 'eleventhemes') ); ?>" title="Eleven WordPress Themes" target="_blank"></a></p>
  </div><!-- // copyright -->   
-     
 </div><!-- // wrap -->   
 
 	<?php wp_footer(); ?>
